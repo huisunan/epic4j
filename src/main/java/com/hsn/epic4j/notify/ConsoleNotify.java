342b3ab3600b544b96775dbf87375cd3f41b0b5e
huisunan <571101320@qq.com>
2022-05-13 08:39:36 +0800

    feat: 修改Console通知的提示

--- src/main/java/com/hsn/epic4j/notify/ConsoleNotify.java
+++ src/main/java/com/hsn/epic4j/notify/ConsoleNotify.java
@@ -5,15 +5,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 @Slf4j
 public class ConsoleNotify implements INotify {
     @Override
     public boolean notifyReceive(List<Item> list) {
-        for (Item item : list) {
-            log.info("success receive game:{}", item.getTitle());
-        }
+        log.info("所有领取到的游戏:{}",list.stream().map(Item::getTitle).collect(Collectors.joining(",")));
         return false;
     }
 }
